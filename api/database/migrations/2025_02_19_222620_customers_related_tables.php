66fd7c63e77c4b4a5e582936901fbc5dcda34eb8
Dananjaya M Perera <dan@macoocoo.com>
2025-02-20 11:33:43 +1300

    add customers db migration

---
+++ api/database/migrations/2025_02_19_222620_customers_related_tables.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // basic customer information table
+        Schema::create('customers', function (Blueprint $table) {
+            // id,first_name,last_name,email,gender
+
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('gender');
+            $table->timestamps();
+        });
+        
+        // customer ip_address record in a separate table. later we can use it to track the ip address changers
+        Schema::create('customers_ip_address', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id');
+            $table->string('ip_address');
+            $table->timestamps();
+        });
+
+        // keep customer company related informations. company,city,title,website
+        Schema::create('customers_companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id');
+            $table->string('company');
+            $table->string('city');
+            $table->string('title');
+            
+            // website can be longer than usual string length of 255. so we use text
+            $table->text('website');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+        Schema::dropIfExists('customers_ip_address');
+        Schema::dropIfExists('customers_companies');
+    }
+};
